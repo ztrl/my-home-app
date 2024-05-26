e86bd487f420e007ed9aa774abc93ab0280f9cb3
SMY <ztr@naver.com>
2024-05-26 20:40:56 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>신문영의 홈페이지</header>
+      <nav>여긴 네비게이션 바</nav>
+      <section>여기가 컨테이너</section>
+      <article>여기가 컨텐츠</article>
+      <aside>여긴 사이드</aside>
+      <footer>여긴 푸터</footer>
+    </>
+  );
+}
+
+export default App;
