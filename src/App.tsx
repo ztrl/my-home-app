c10191e17a2054be3052887d6b4b20562726f6ef
신문영 <ztrl@naver.com>
2024-05-27 12:36:24 +0900

    feat: Add project contents and favicon, title

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ function App() {
           <img
             className="w-[300px] h-[400px] mr-28"
             src="/images/profile.jpg"
-            alt=""
+            alt="my_profile"
           />
         </div>
-        <div className="w-full text-start bg-gray-700 p-10">
+        <div className="w-full text-start text-white bg-gray-700 p-10">
           <div className="text-5xl font-bold">신문영 (1997.01.29, 男)</div>
           <div className="text-3xl mt-10">Backend Developer</div>
           <div className="text-end text-lg mt-28">Email: ztrl@naver.com</div>
@@ -63,7 +63,7 @@ function App() {
             </section>
           </article>
           <article className="text-black text-start p-7">
-            <header className="p-7 font-bold">SKILL</header>
+            <header className="p-7 font-bold">SKILL (WIP)</header>
             <hr />
             <section className="p-7">
               <header className="text-xl font-semibold rounded-lg bg-blue-300 w-fit px-2">
@@ -137,7 +137,7 @@ function App() {
                 <header className="text-2xl pb-3">Docker</header>
                 <p>
                   - Docker 컨테이너를 띄워 주변 환경에 독립적인 어플리케이션을
-                  만들어본 적이 있습니다.
+                  만들어 본 적이 있습니다.
                 </p>
               </article>
               <article className="py-5">
@@ -161,10 +161,7 @@ function App() {
               </header>
               <article className="py-5">
                 <header className="text-2xl pb-3">Git</header>
-                <p>
-                  - Git을 활용한 기초적인 형상관리 작업을 할 수 있습니다. (pull,
-                  merge)
-                </p>
+                <p>- 기초적인 형상관리 작업을 할 수 있습니다. (pull, merge)</p>
                 <p>
                   - 원활한 커뮤니케이션과 소스 변경 이력을 쉽게 확인하기 위해
                   Commit convention을 수립하고 이를 지키는데 관심이 많습니다.
@@ -173,12 +170,105 @@ function App() {
               <article className="py-5">
                 <header className="text-2xl pb-3">Jira</header>
                 <p>
-                  - Jira에서 티켓을 발급해 기초적인 이슈 관리를 할 수 있습니다.
+                  - Jira 티켓을 발급해서 기초적인 이슈 관리를 할 수 있습니다.
                 </p>
               </article>
               <article className="py-5">
                 <header className="text-2xl pb-3">Swagger</header>
-                <p>- Swagger로 API 명세 하는 것을 선호합니다.</p>
+                <p>
+                  - Swagger로 API 명세를 해서 프론트엔드 개발자와 협업 하는 것을
+                  선호합니다.
+                </p>
+              </article>
+            </section>
+          </article>
+          <article className="text-black text-start p-7">
+            <header className="p-7 font-bold">PROJECT (WIP)</header>
+            <hr />
+            <section className="p-7">
+              <article className="flex flex-col py-10">
+                <img
+                  className="w-[400px] h-[200px] self-center"
+                  src="https://raw.githubusercontent.com/ztrl/petcong/master/asset/logo.png"
+                  alt="petcong_thumbnail"
+                  onClick={() =>
+                    (location.href = "https://github.com/ztrl/petcong")
+                  }
+                />
+                <header className="self-center py-5">
+                  <a
+                    className="text-2xl font-bold  text-blacks"
+                    href="https://github.com/ztrl/petcong"
+                  >
+                    Petcong
+                  </a>
+                </header>
+                <div className="self-center text-center py-3">
+                  <p className="p-3 font-bold">
+                    반려동물 실시간 랜덤 소개팅 모바일 어플리케이션
+                  </p>
+                  <p className="p-3">- Role: backend & infra developement</p>
+                  <p className="p-3">
+                    - Backend contribute: Member API 구현 및 Spring Security를
+                    활용한 인증
+                  </p>
+                  <p className="p-3">
+                    - Infra contribute: Jenkins를 사용해 CI/CD 파이프라인 구축
+                  </p>
+                </div>
+              </article>
+              <article className="flex flex-col py-10">
+                <img
+                  className="w-[200px] h-[200px] self-center"
+                  src="https://private-user-images.githubusercontent.com/65946607/327988217-786a6907-7406-4f2f-8c18-58d0ee9dbb73.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTY3NzMxNzAsIm5iZiI6MTcxNjc3Mjg3MCwicGF0aCI6Ii82NTk0NjYwNy8zMjc5ODgyMTctNzg2YTY5MDctNzQwNi00ZjJmLThjMTgtNThkMGVlOWRiYjczLmpwZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA1MjclMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNTI3VDAxMjExMFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWNmN2I3YTBmZWE3MzI1MDA3ODI3YTgwZTYzZWE5ZjUzMDJjMjlkNzkzYzBhZmM3MmJiOTA3ODVhYjc3MTZiZjQmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.YeCdFftG0FbQUjQZEJM6wIPpvB4xOPcnIIPD6sa1a5g"
+                  alt="findear_thumbnail"
+                  onClick={() =>
+                    (location.href = "https://github.com/2TF4/findear")
+                  }
+                />
+                <header className="self-center py-5">
+                  <a
+                    className="text-2xl font-bold  text-black"
+                    href="https://github.com/2TF4/findear"
+                  >
+                    Findear
+                  </a>
+                </header>
+                <div className="self-center text-center py-3">
+                  <p className="p-3 font-bold">
+                    분실물 통합 관리 플랫폼 Findear
+                  </p>
+                  <p className="p-3">- Role: frontend developement</p>
+                  <p className="p-3">- contribute: 메인화면 구현</p>
+                </div>
+              </article>
+              <article className="flex flex-col py-10">
+                <img
+                  className="rounded-3xl w-[200px] h-[200px] self-center"
+                  src="https://raw.githubusercontent.com/4-EVERY1/QQueueing/master/.asset/queueing.png"
+                  alt="qqueueing_thumbnail"
+                  onClick={() =>
+                    (location.href = "https://github.com/4-EVERY1/QQueueing")
+                  }
+                />
+                <header className="self-center py-5">
+                  <a
+                    className="text-2xl font-bold  text-black"
+                    href="https://github.com/4-EVERY1/QQueueing"
+                  >
+                    QQueueing
+                  </a>
+                </header>
+                <div className="self-center text-center py-3">
+                  <p className="p-3 font-bold">오픈소스 대기열 서비스</p>
+                  <p className="p-3">- Role: backend developement</p>
+                  <p className="p-3">
+                    - contribute: jmeter 테스트 계획 수립 &
+                    <br />
+                    사용자 서버와 QQueueing에서 제공하는 대기열 서버를 연결하는
+                    Adapter 프로토타입 개발
+                  </p>
+                </div>
               </article>
             </section>
           </article>
@@ -186,7 +276,7 @@ function App() {
       </div>
       {/* <aside>여긴 사이드</aside> */}
       <footer className="bg-white text-black text-5xl font-bold p-10">
-        * 포트폴리오 내용은 현재 업데이트 중 입니다.
+        * 포트폴리오는 현재 업데이트 중 입니다.
       </footer>
     </>
   );
